d137931f4e57139f01fc838ba09dc2444c1f1d14
Nick Stathas <nistath@mit.edu>
2021-05-13 02:24:25 -0400

    Add timing for precompute

--- src/spline_racer/src/main.cpp
+++ src/spline_racer/src/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <fstream>
 #include <iostream>
 
@@ -15,19 +16,25 @@ Path make_path(Track::const_iterator begin, Track::const_iterator end) {
 
 int main() {
   std::ifstream track_csv("launch/maps/lvms/track.csv");
+
+  auto track_start = std::chrono::high_resolution_clock::now();
   auto track = PrecomputedTrack(load_track(track_csv));
   track_csv.close();
+  auto track_stop = std::chrono::high_resolution_clock::now();
+  std::cout << "Track loaded and precomputed in "
+            << std::chrono::duration<float>(track_stop - track_start).count()
+            << " seconds\n";
 
   Objects objects(
       {{track, 0,
         Box{-102.8, 13.9, M_PI / 2 + M_PI / 8 - 2.566, 5, 2}.dilated(1.2), 30},
        {track, 0, Box{-97.0, 13., M_PI / 6 - 2.566, 1.0, 1.0}, -10}});
 
-  for (const auto& obj : objects) {
-    std::cout << "Object with cost: " << obj.cost
+  for (const auto& object : objects) {
+    std::cout << "Object with cost: " << object.cost
               << " has corners on track indices ";
 
-    for (const auto& corner : obj.corners) {
+    for (const auto& corner : object.corners) {
       std::cout << corner.it.idx() << " ";
     }
     std::cout << "\n";

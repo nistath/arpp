c11cd1a5bf3618d90ce4224706622528cb5dbc66
Nick Stathas <nistath@mit.edu>
2021-05-13 03:29:57 -0400

    More complicated example and readme

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Spline Racer for Autonomous Racing Path Planning
 
-Spline racer is a flavor of an autonomous racing path planner, invented by by Chris Chang <cwkchang@mit.edu>
+Spline racer is a flavor of an autonomous racing path planner, invented and developed by [Chris Chang](mailto:cwkchang@mit.edu), [Nick Stathas](mailto:nistath@mit.edu), and [MIT Driverless](https://driverless.mit.edu) team members.
 
-This repository contains a simplified implementation from scratch.
+This repository contains a simplified, but well-engineered, implementation from scratch by Nick.
 Please refer to the accompanying project report for an overview of the concepts.
 
 ## List of Limitations
@@ -37,4 +37,19 @@ source ./install/setup.bash
 4. Run the executable
 ```
 ros2 run spline_racer main
+```
+Example output:
+```
+Track loaded and precomputed in 4.34146 seconds
+Object with cost: -20 has corners on track indices 202 203 203 204
+Object with cost: 30 has corners on track indices 205 208 212 215
+Object with cost: -50 has corners on track indices 217 218 218 219
+Default path has travel cost 38.9929
+Default path has total cost 68.9929
+New best path has total cost 49.2058 with decisions: (Object Cost: -20, Decision: Right)
+New best path has total cost 49.1199 with decisions: (Object Cost: -20, Decision: Collide)
+New best path has total cost 49.0775 with decisions: (Object Cost: -20, Decision: Left)
+New best path has total cost 19.4591 with decisions: (Object Cost: -20, Decision: Left) (Object Cost: 30, Decision: Left)
+New best path has total cost 3.51051 with decisions: (Object Cost: -20, Decision: Left) (Object Cost: 30, Decision: Left) (Object Cost: -50, Decision: Collide)
+New best path has total cost 2.96748 with decisions: (Object Cost: -20, Decision: Left) (Object Cost: 30, Decision: Left) (Object Cost: -50, Decision: Right)
 ```
--- src/spline_racer/src/main.cpp
+++ src/spline_racer/src/main.cpp
@@ -18,7 +18,7 @@ int main() {
   std::ifstream track_csv("launch/maps/lvms/track.csv");
 
   auto track_start = std::chrono::high_resolution_clock::now();
-  auto track = PrecomputedTrack(load_track(track_csv));
+  auto track = Track(load_track(track_csv));
   track_csv.close();
   auto track_stop = std::chrono::high_resolution_clock::now();
   std::cout << "Track loaded and precomputed in "
@@ -28,9 +28,13 @@ int main() {
   Objects objects(
       {{track, 0,
         Box{-102.8, 13.9, M_PI / 2 + M_PI / 8 - 2.566, 2, 2}.dilated(1.2), 30},
-       {track, 0, Box{-97.0, 13., M_PI / 6 - 2.566, 1, 1}, -10}});
+       {track, 0, Box{-97.0, 13., M_PI / 6 - 2.566, 1, 1}, -50},
+       {track, 0, Box{-106.0, 16., M_PI / 6 - 2.566, 1, 1}, -20}});
 
-  for (const auto& object : objects) {
+  Path default_path = make_path(track.cbegin() + 180, track.cbegin() + 259);
+  Search search(std::move(objects), default_path);
+
+  for (const auto& object : search.get_objects()) {
     std::cout << "Object with cost: " << object.cost
               << " has corners on track indices ";
 
@@ -40,8 +44,6 @@ int main() {
     std::cout << "\n";
   }
 
-  Path default_path = make_path(track.cbegin() + 202, track.cbegin() + 259);
-  Search search(std::move(objects), default_path);
   std::cout << "Default path has travel cost "
             << calculate_travel_cost(default_path) << std::endl;
   std::cout << "Default path has total cost " << search.get_best_cost()
@@ -55,7 +57,8 @@ int main() {
                 << " with decisions: ";
       auto best_decisions = search.get_best_decisions();
       for (size_t i = 0; i < best_decisions.size(); ++i) {
-        std::cout << "(Object Cost: " << search.get_objects()[i].cost << ", Decision: ";
+        std::cout << "(Object Cost: " << search.get_objects()[i].cost
+                  << ", Decision: ";
         switch (best_decisions[i]) {
           case Search::Decision::COLLIDE:
             std::cout << "Collide";
